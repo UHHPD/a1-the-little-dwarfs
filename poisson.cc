76b682a33a215808aff4fbf640c48df54ee157d0
Gamingtics <102976891+Gamingtics@users.noreply.github.com>
2022-06-20 15:21:04 +0000

    1 c

--- poisson.cc
+++ poisson.cc
@@ -1,19 +1,23 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <cmath>
+double poisson(double mu, int k){
+  return pow(mu,k)*exp(-mu)/tgamma(k+1);
+}
 
 int main(void){
 std::vector<int> zaehler(11); 
 
 std::ifstream fin("datensumme.txt");
-std::ofstream fout("hist.txt");
+std::ofstream fout("histpoi.txt");
 int a;
 while (fin >> a) {
   zaehler[a]+=1;
      }
   fin.close();
   for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) {
-  fout << k << " " << zaehler [ k ] << std :: endl ;
+  fout << k << " " << zaehler [ k ] << " " << poisson(3.11538, k)*234 << std :: endl ;
 }
   return 0;
   }
