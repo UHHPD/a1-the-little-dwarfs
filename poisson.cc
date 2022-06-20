b54a87c14feb7b02a49efb84ad3d2398e23a69b4
Gamingtics <102976891+Gamingtics@users.noreply.github.com>
2022-06-20 14:57:00 +0000

    1 b

--- poisson.cc
+++ poisson.cc
@@ -6,13 +6,14 @@ int main(void){
 std::vector<int> zaehler(11); 
 
 std::ifstream fin("datensumme.txt");
+std::ofstream fout("hist.txt");
 int a;
 while (fin >> a) {
   zaehler[a]+=1;
      }
   fin.close();
   for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) {
-std :: cout << k << ":" << zaehler [ k ] << std :: endl ;
+  fout << k << " " << zaehler [ k ] << std :: endl ;
 }
   return 0;
   }
